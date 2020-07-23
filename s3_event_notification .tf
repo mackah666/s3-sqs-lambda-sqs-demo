e6641af5b6d4d9c1a3636ba6193deb4895cb3937
mackah666 <mackah666@yahoo.co.uk>
2020-07-23 14:47:04 +0100

    Added use of output var

--- s3_event_notification .tf
+++ s3_event_notification .tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = "mackah-s3-terraform-bucket"
 
   queue {
-    queue_arn     = "${aws_sqs_queue.queue.arn}"
+    queue_arn     = "${sqs_queue.arn}"
     events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
+    filter_suffix = ".*"
   }
 }

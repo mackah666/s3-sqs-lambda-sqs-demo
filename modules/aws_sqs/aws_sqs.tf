9644dd5222b3d9fca3bcbc0e836094a6e4a17aa5
mackah666 <mackah666@yahoo.co.uk>
2020-07-23 15:10:59 +0100

    Added module folder

--- modules/aws_sqs/aws_sqs.tf
+++ modules/aws_sqs/aws_sqs.tf
@@ -14,6 +14,6 @@ resource "aws_sqs_queue" "terraform_queue" {
   }
 }
 output "sqs_queue_arn" {
-  value       = "${aws_sqs_queue.queue.arn}"
+  value       = "${aws_sqs_queue.terraform_queue.arn}"
   description = "The Id of the security group created."
 }
--- s3_event_notification .tf
+++ s3_event_notification .tf
@@ -1,8 +1,10 @@
+variable "sqs_queue_arn" {}
+
 resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = "mackah-s3-terraform-bucket"
 
   queue {
-    queue_arn     = "${sqs_queue.arn}"
+    queue_arn     = var.sqs_queue_arn
     events        = ["s3:ObjectCreated:*"]
     filter_suffix = ".*"
   }
